e03ee9be8e9905199938745883354092e964b6c2
Oscar Jaimes <oscar@jaimes.io>
2023-05-09 23:35:57 -0600

    Contrain Map To Alberta (#12)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Layout from '@/components/Layout';
 export default function Home() {
   return (
     <Layout>
-      <Map lat={53.52904} lng={-113.48899} zoom={10} />
+      <Map lat={53.52904} lng={-113.48899} zoom={7} />
     </Layout>
   )
 }

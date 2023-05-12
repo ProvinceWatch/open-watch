ff801ff0dfbdfc58c3ba502d41923d9b4276fb1c
ajboman <89932630+ajboman@users.noreply.github.com>
2023-05-12 11:51:45 -0600

    Refactor cameras (#24)
    * cameras page started
    
    * cameras data sorted on received
    
    * change size of grid
    
    * fix navbar stickyness
    
    * Refactored Code and Added Reload Interval
    
    * rewrote function
    
    * message
    
    ---------
    
    Co-authored-by: Oscar Jaimes <oscar@jaimes.io>

---
+++ src/app/cameras/sort.tsx
@@ -0,0 +1,68 @@
+import { useState, useEffect } from 'react';
+import axios, { AxiosResponse } from 'axios';
+import { CameraData, CameraResponse } from '@/app/map/defs';
+import { Section } from '@/components/CameraGrid';
+
+
+const BOUNDARIES = {
+  'calgary-cameras': { latMin: 50.8345, latMax: 51.19477, lonMin: -114.2705, lonMax: -113.79533 },
+  'edmonton-cameras': { latMin: 53.39612, latMax: 53.66752, lonMin: -113.7123, lonMax: -113.2454 },
+  'banff-cameras': { latMin: 51.0279, latMax: 51.5040, lonMin: -116.3205, lonMax: -115.10374 },
+  'alberta-highways': { latMin: 49, latMax: 60, lonMin: -120, lonMax: -110 },
+};
+
+export function useSortedCameras() {
+  const [cameras, setCameras] = useState<{
+    [key in 'calgary-cameras' | 'edmonton-cameras' | 'banff-cameras' | 'alberta-highways']: CameraData[];
+  }>({
+    'calgary-cameras': [],
+    'edmonton-cameras': [],
+    'banff-cameras': [],
+    'alberta-highways': [],
+  });
+
+  useEffect(() => {
+    const fetchAndSortCameras = async () => {
+      try {
+        const cameraResponse: AxiosResponse<CameraResponse> = await axios.get('/map/cameras');
+        const cameraData: CameraData[] = (cameraResponse.data.data as any) as CameraData[];
+
+        const categorizedCameras: Record<Section, CameraData[]> = {
+            'calgary-cameras': [],
+            'edmonton-cameras': [],
+            'banff-cameras': [],
+            'alberta-highways': [],
+          };
+          
+
+        for (const camera of cameraData) {
+          for (const section in BOUNDARIES) {
+            const { latMin, latMax, lonMin, lonMax } = BOUNDARIES[section as Section];
+            if (
+              camera.Latitude >= latMin &&
+              camera.Latitude <= latMax &&
+              camera.Longitude >= lonMin &&
+              camera.Longitude <= lonMax
+            ) {
+                categorizedCameras[section as Section].push(camera);
+                break;
+            }
+          }
+        }
+
+        setCameras(categorizedCameras);
+      } catch (error) {
+        console.error('Failed to fetch and sort camera data:', error);
+      }
+    };
+
+    fetchAndSortCameras();
+    const intervalId = setInterval(fetchAndSortCameras, 5 * 60 * 1000); // Fetch new data every 5 minutes
+
+    return () => {
+      clearInterval(intervalId); // Clear the interval when the component is unmounted
+    };
+  }, []);
+
+  return cameras;
+}
--- src/components/CameraGrid.tsx
+++ src/components/CameraGrid.tsx
@@ -1,16 +1,8 @@
-// CameraGrid.tsx
-import React, { useEffect, useState } from 'react';
-import axios, { AxiosResponse } from 'axios';
-import { CameraData, CameraResponse } from '@/app/map/defs';
+import React from 'react';
+import { CameraData } from '@/app/map/defs';
+import { useSortedCameras } from '@/app/cameras/sort';
 
-type Section = 'alberta-highways' | 'calgary-cameras' | 'edmonton-cameras' | 'banff-cameras';
-
-const BOUNDARIES = {
-  'calgary-cameras': { latMin: 50.8345, latMax: 51.19477, lonMin: -114.2705, lonMax: -113.79533 },
-  'edmonton-cameras': { latMin: 53.39612, latMax: 53.66752, lonMin: -113.7123, lonMax: -113.2454 },
-  'banff-cameras': { latMin: 51.0279, latMax: 51.5040, lonMin: -116.3205, lonMax: -115.10374 },
-  'alberta-highways': { latMin: 49, latMax: 60, lonMin: -120, lonMax: -110 },
-};
+export type Section = 'alberta-highways' | 'calgary-cameras' | 'edmonton-cameras' | 'banff-cameras';
 
 interface CameraGridProps {
   section: Section;
@@ -18,51 +10,7 @@ interface CameraGridProps {
 }
 
 const CameraGrid: React.FC<CameraGridProps> = ({ section, gridSize }) => {
-  const [cameras, setCameras] = useState<{
-    [key in Section]: CameraData[];
-  }>({
-    'calgary-cameras': [],
-    'edmonton-cameras': [],
-    'banff-cameras': [],
-    'alberta-highways': [],
-  });
-
-  useEffect(() => {
-    const fetchCameras = async () => {
-      try {
-        const cameraResponse: AxiosResponse<CameraResponse> = await axios.get('/map/cameras');
-        const cameraData: CameraData[] = (cameraResponse.data.data as any) as CameraData[];
-
-        const categorizedCameras = {
-          'calgary-cameras': [],
-          'edmonton-cameras': [],
-          'banff-cameras': [],
-          'alberta-highways': [],
-        };
-
-        for (const camera of cameraData) {
-          for (const section in BOUNDARIES) {
-            const { latMin, latMax, lonMin, lonMax } = BOUNDARIES[section];
-            if (
-              camera.Latitude >= latMin &&
-              camera.Latitude <= latMax &&
-              camera.Longitude >= lonMin &&
-              camera.Longitude <= lonMax
-            ) {
-              categorizedCameras[section as Section].push(camera);
-              break;
-            }
-          }
-        }
-
-        setCameras(categorizedCameras);
-      } catch (error) {
-        console.error('Failed to fetch camera data:', error);
-      }
-    };
-
-    fetchCameras();
-  }, []);
+  const cameras = useSortedCameras();
 
   const cameraName = (camera: CameraData) => {
     if (camera.Name) {

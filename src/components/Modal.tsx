1b8e179905f07a5d75e4ec7b20db040392dc323b
Adam Boman <adamboman1@hotmail.com>
2023-05-13 11:05:42 -0600

    camera-grid UI changes

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
-// can use this modal anywhere
-// see CameraGrid.tsx return statement for opening a new modal.
+
 interface ModalProps {
   open: boolean;
   onClose: () => void;
@@ -20,11 +19,17 @@ const Modal: React.FC<ModalProps> = ({ open, onClose, children }) => {
     return null;
   }
 
-  // change the style of modal here.
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50">
       <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50" onClick={onClose} />
-      <div className="bg-white p-6 rounded shadow-lg max-w-lg mx-auto z-50 overflow-y-auto">
+      <div className="bg-white p-6 rounded shadow-lg max-w-lg mx-auto z-50 overflow-y-auto relative">
+        <button 
+          onClick={onClose} 
+          className="absolute right-3 top-3 text-gray-700 hover:text-gray-900 transition-colors duration-200"
+          aria-label="Close Modal"
+        >
+          X
+        </button>
         {children}
       </div>
     </div>

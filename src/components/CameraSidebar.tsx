b5f68933b747f206dbbb1fdf2c2287399850a9e5
Adam Boman <adamboman1@hotmail.com>
2023-05-13 11:48:03 -0600

    uses flowbite and highlights selected

--- src/components/CameraSidebar.tsx
+++ src/components/CameraSidebar.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ListGroup } from 'flowbite-react';
 
 type Section = 'alberta-highways' | 'calgary-cameras' | 'edmonton-cameras' | 'banff-cameras';
 
@@ -9,14 +10,29 @@ interface CameraSidebarProps {
 }
 
 const CameraSidebar = ({ onSectionSelect, onIncreaseGridSize, onDecreaseGridSize }: CameraSidebarProps) => {
+  const sections: Section[] = ['alberta-highways', 'calgary-cameras', 'edmonton-cameras', 'banff-cameras'];
+  
+  const [activeSectionIndex, setActiveSectionIndex] = useState(0);
+
+  const handleSelect = (index: number) => {
+    setActiveSectionIndex(index);
+    onSectionSelect(sections[index]);
+  };
+
   return (
     <div className="w-64 min-h-screen bg-gray-100 p-4 fixed left-0">
-      <ul className="mt-8">
-        <li className="py-2 px-4 bg-gray-200 hover:bg-gray-300 cursor-pointer text-black" onClick={() => onSectionSelect('alberta-highways')}>Alberta Highways</li>
-        <li className="py-2 px-4 bg-gray-200 hover:bg-gray-300 cursor-pointer text-black" onClick={() => onSectionSelect('calgary-cameras')}>Calgary Cameras</li>
-        <li className="py-2 px-4 bg-gray-200 hover:bg-gray-300 cursor-pointer text-black" onClick={() => onSectionSelect('edmonton-cameras')}>Edmonton Cameras</li>
-        <li className="py-2 px-4 bg-gray-200 hover:bg-gray-300 cursor-pointer text-black" onClick={() => onSectionSelect('banff-cameras')}>Banff Cameras</li>
-      </ul>
+      <ListGroup>
+        {sections.map((section, index) => (
+          <ListGroup.Item 
+            key={section}
+            onClick={() => handleSelect(index)}
+            action
+            active={index === activeSectionIndex} // Highlight if this section is selected
+          >
+            {section.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')}
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
       <div className="flex justify-between mt-4">
         <button
           className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
@@ -35,4 +51,4 @@ const CameraSidebar = ({ onSectionSelect, onIncreaseGridSize, onDecreaseGridSize
   );
 };
 
-export default CameraSidebar;
+export default React.memo(CameraSidebar);

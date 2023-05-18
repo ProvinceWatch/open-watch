b65e964f36eaa1cc00c0400c06db857b2690851d
Adam Boman <adamboman1@hotmail.com>
2023-05-17 19:14:05 -0600

    added loading spinner

--- src/components/cameras/CameraGrid.tsx
+++ src/components/cameras/CameraGrid.tsx
@@ -3,6 +3,7 @@ import { CameraData } from '@/app/map/defs';
 import { sortCameras } from '@/app/cameras/sort';
 import { Section } from '@/app/cameras/defs';
 import CameraModal from '@/components/cameras/CameraModal';
+import { Spinner } from 'flowbite-react';
 
 interface CameraGridProps {
   section: Section;
@@ -16,11 +17,13 @@ const CameraGrid: React.FC<CameraGridProps> = ({ section, gridSize }) => {
     'banff-cameras': [],
     'alberta-highways': [],
   });
-  const [selectedCamera, setSelectedCamera] = useState<CameraData | null>(null); // null when no camera is selected
+  const [selectedCamera, setSelectedCamera] = useState<CameraData | null>(null);
+  const [loadedImages, setLoadedImages] = useState<Record<string, boolean>>({});
 
   useEffect(() => {
     const fetchAndSortCameras = async () => {
       try {
+        setLoadedImages({}); // Reset the loaded images state
         const res: any = await fetch('/map/cameras');
         const cameraResponse = await res.json();
         let cameraData: CameraData[] = (cameraResponse.data as any) as CameraData[];
@@ -57,15 +60,30 @@ const CameraGrid: React.FC<CameraGridProps> = ({ section, gridSize }) => {
     <div className={`grid ${gridSize} gap-4 px-3 text-black overflow-auto max-h-screen`}>
       {cameras[section].map((camera, index) => (
         <div key={index} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" onClick={() => setSelectedCamera(camera)}>
-          <a href="#">
-            <img className="rounded-t-lg w-full h-auto" src={camera.Url} alt="Camera Snapshot" />
-          </a>
-          <div className="p-5">
-            <a href="#">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{cameraName(camera)}</h5>
-            </a>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Camera details...</p>
-          </div>
+          {!loadedImages[camera.Url] ? (
+            <div role="status" className="relative w-full aspect-[1/1]">
+              <div className="absolute inset-0 flex items-center justify-center">
+                <Spinner size="xl"/>
+              </div>
+            </div>
+          ) : null}
+          <img
+            style={{ display: loadedImages[camera.Url] ? 'block' : 'none' }}
+            className="rounded-t-lg w-full h-auto"
+            src={camera.Url}
+            alt="Camera Snapshot"
+            onLoad={() => setLoadedImages((prev) => ({ ...prev, [camera.Url]: true }))}
+          />
+          {loadedImages[camera.Url] && (
+            <div className="p-5">
+              <a href="#">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  {cameraName(camera)}
+                </h5>
+              </a>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Click for details...</p>
+            </div>
+          )}
         </div>
       ))}
       {selectedCamera && (

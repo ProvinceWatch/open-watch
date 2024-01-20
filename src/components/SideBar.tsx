f4c97fd41f371fa316b1ce9ce0d2bace818d3304
Oscar Jaimes <oscar@jaimes.io>
2024-01-19 18:53:50 -0700

    Home Page + Map UI

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -21,7 +21,7 @@ export const SideBar = ({ handleToggleSidebar, isOpen, children, pt }: SideBarPr
       <div style={{ width: '35px', height: '35px', backgroundColor: 'white', zIndex: 4, position: 'fixed', borderRadius: '8px', textAlign: 'center' }}>
         <TbLayoutSidebar onClick={handleToggleSidebar} size={35} style={{ position: 'fixed', zIndex: 4, marginLeft: `${isOpen ? '1%' : '0%'}`, color: 'black' }} />
       </div>
-      <div className={`w-70 min-h-screen bg-white p-3 pt-10 fixed transform transition-transform duration-300 ${isOpen ? '' : 'translate-x-80'}`} style={{ zIndex: 3, display: 'flex', flexDirection: 'column', height: '100%' }}>
+      <div className={`w-3/12 min-h-screen bg-white p-3 pt-10 fixed transform transition-transform duration-300 ${isOpen ? '' : 'translate-x-80'}`} style={{ zIndex: 3, display: 'flex', flexDirection: 'column', height: '100%' }}>
         {children}
       </div>
     </>

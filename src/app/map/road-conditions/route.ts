f32da6da8e9f51c1cc7e35389eb74cf9c9c7c38e
Oscar Jaimes <oscar@jaimes.io>
2024-01-21 12:18:54 -0700

    wip

--- src/app/map/road-conditions/route.ts
+++ src/app/map/road-conditions/route.ts
@@ -3,6 +3,5 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   const res = await fetch('https://511.alberta.ca/api/v2/get/winterroads', { cache: 'no-store' });
   const data = await res.json();
-  console.log(data);
   return NextResponse.json({ data });
 }
--- src/app/map/weather-alerts/route.ts
+++ src/app/map/weather-alerts/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   try {
-    const res = await fetch('https://weather.gc.ca/data/dms/alert_geojson/alerts.en.geojson');
+    const res = await fetch('https://weather.gc.ca/data/dms/alert_geojson/alerts.en.geojson', { cache: 'no-store' });
     const data = await res.json();
     return NextResponse.json({ data });
   } catch (error) {

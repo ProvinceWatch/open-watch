1b767a189c56105fccff5e32b80cece627ac292f
Oscar Jaimes <oscar@jaimes.io>
2024-01-21 12:39:36 -0700

    Testing

--- src/app/map/weather-alerts/route.ts
+++ src/app/map/weather-alerts/route.ts
@@ -4,8 +4,11 @@ export async function GET() {
   try {
     const res = await fetch('https://weather.gc.ca/data/dms/alert_geojson/alerts.en.geojson', { cache: 'no-store' });
     const data = await res.json();
+    console.log('DATA FROM WEATHER ALERTS HERE');
+    console.log(data);
     return NextResponse.json({ data });
   } catch (error) {
+    console.log(error);
     return NextResponse.json({ data: [] })
   }
 }

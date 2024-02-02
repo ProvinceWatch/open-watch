ee0fb905e4262ff80d3a56d857d623bdfaf7d018
Oscar Jaimes <oscar@jaimes.io>
2024-02-02 14:38:00 -0700

    Fix Alert Requests (#89)

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,3 +1,9 @@
-export const fetchWeather             = () => fetch('/api/weather').then(res => res.json())
-export const fetchAlbertaAlerts       = () => fetch('/api/ab-alerts').then(res => res.json())
-export const fetchCanadaWeatherAlerts = () => fetch('/api/can-weather-alerts', { cache: 'no-store' }).then(res => res.json())
+export const fetchWeather = () => fetch('/api/weather').then(res => res.json())
+
+export const fetchAlbertaAlerts = () => fetch('/api/ab-alerts').then(res => res.json())
+
+export const fetchCanadaWeatherAlerts = () => fetch('/api/can-weather-alerts', {
+  headers: {
+    'Cache-Control': 'no-cache'
+  }
+}).then(res => res.json());
